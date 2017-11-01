dd97b3b9aa52ade562ab0611afb8e547435ec3c6
jvican <jorgevc@fastmail.es>
2017-11-01 19:44:37 +0100

    Execute clean cache

--- bin/clean-cache.sh
+++ bin/clean-cache.sh
@@ -1,5 +1,6 @@
-#!/bin/bash - 
-set -eou
+#!/usr/bin/env bash
+set -eu
+set -o nounset
 
 find "$HOME/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
 find "$HOME/.coursier/cache" -name "ivydata-*.properties" -print -delete
