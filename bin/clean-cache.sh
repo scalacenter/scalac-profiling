12a82bf882c318ba60127a81b9ca0140d0ac2fbc
jvican <jorgevc@fastmail.es>
2017-11-02 10:40:16 +0100

    Remove coursier from the clean cache script

--- bin/clean-cache.sh
+++ bin/clean-cache.sh
@@ -3,5 +3,4 @@ set -eu
 set -o nounset
 
 find "/drone/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
-find "/drone/.coursier/cache" -name "ivydata-*.properties" -print -delete
 find "/drone/.sbt"            -name "*.lock"               -print -delete
