ef2802bde076b51ec716ab77b5cb1618f7463811
jvican <jorgevc@fastmail.es>
2017-11-07 15:24:34 +0100

    Try to trick the CI

--- bin/clean-cache.sh
+++ bin/clean-cache.sh
@@ -2,5 +2,11 @@
 set -eu
 set -o nounset
 
+ls -al /drone
+
+ls -al /root/.ivy2
+ls -al /root/.coursier
+ls -al /root/.sbt
+
 find "/root/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
 find "/root/.sbt"            -name "*.lock"               -print -delete
