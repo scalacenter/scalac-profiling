2a2209fa2409c85c7d5c39f8c4c349eb3254d14f
jvican <jorgevc@fastmail.es>
2017-11-07 15:46:49 +0100

    Remove ivydata in .coursier

--- bin/clean-cache.sh
+++ bin/clean-cache.sh
@@ -2,23 +2,6 @@
 set -eu
 set -o nounset
 
-echo "DRONE"
-ls -al /drone
-echo "DRONE IVY"
-ls -al /drone/.ivy2
-echo "DRONE COURSIER"
-ls -al /drone/.coursier
-echo "DRONE SBT"
-ls -al /drone/.sbt
-
-echo "ROOT"
-ls -al /root
-echo "ROOT IVY"
-ls -al /root/.ivy2
-echo "ROOT COURSIER"
-ls -al /root/.coursier
-echo "ROOT SBT"
-ls -al /root/.sbt
-
 find "/root/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
+find "/root/.coursier"       -name "ivydata-*.properties" -print -delete
 find "/root/.sbt"            -name "*.lock"               -print -delete
