33525b4b990a1e17fcea064522e9d48363e3ee28
jvican <jorgevc@fastmail.es>
2017-11-07 15:04:50 +0100

    Use /root instead of /drone

--- bin/clean-cache.sh
+++ bin/clean-cache.sh
@@ -2,13 +2,5 @@
 set -eu
 set -o nounset
 
-find "/drone/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
-find "/drone/.sbt"            -name "*.lock"               -print -delete
-
-if [[ -d "/drone/.coursier/cache" ]]; then
-  find "/drone/.coursier/cache" -name "ivydata-*.properties" -print -delete
-fi
-
-if [[ -d "$HOME/.coursier/cache" ]]; then
-  find "$HOME/.coursier/cache" -name "ivydata-*.properties" -print -delete
-fi
+find "/root/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
+find "/root/.sbt"            -name "*.lock"               -print -delete
