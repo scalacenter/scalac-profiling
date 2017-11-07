5c0c45e9dbff8802d92f40202f71b40dcc69efc3
jvican <jorgevc@fastmail.es>
2017-11-07 15:36:23 +0100

    Add printlns to script

--- bin/clean-cache.sh
+++ bin/clean-cache.sh
@@ -2,10 +2,22 @@
 set -eu
 set -o nounset
 
+echo "DRONE"
 ls -al /drone
+echo "DRONE IVY"
+ls -al /drone/.ivy2
+echo "DRONE COURSIER"
+ls -al /drone/.coursier
+echo "DRONE SBT"
+ls -al /drone/.sbt
 
+echo "ROOT"
+ls -al /root
+echo "ROOT IVY"
 ls -al /root/.ivy2
+echo "ROOT COURSIER"
 ls -al /root/.coursier
+echo "ROOT SBT"
 ls -al /root/.sbt
 
 find "/root/.ivy2/cache"     -name "ivydata-*.properties" -print -delete
