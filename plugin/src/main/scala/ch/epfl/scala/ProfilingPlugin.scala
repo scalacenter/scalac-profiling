513a20f8b374d66b9a8c43fe80833d83e528ef7c
danicheg <esik.danny@gmail.com>
2023-11-28 20:39:23 +0300

    Sort compiler options out in optionsHelp

--- plugin/src/main/scala/ch/epfl/scala/ProfilingPlugin.scala
+++ plugin/src/main/scala/ch/epfl/scala/ProfilingPlugin.scala
@@ -79,21 +79,21 @@ class ProfilingPlugin(val global: Global) extends Plugin { self =>
        |-P:$name:${pad20(
         GenerateGlobalFlamegraph
       )}: Creates a global flamegraph of implicit searches for all compilation units. Use the `-P:$name:$SourceRoot` option to manage the root directory, otherwise, a working directory (defined by the `user.dir` property) will be picked.
-       |-P:$name:${pad20(SourceRoot)}:_ Sets the source root for this project.
-       |-P:$name:${pad20(GenerateProfileDb)}:_ Sets the source root for this project.
-       |-P:$name:${pad20(ShowProfiles)} Logs profile information for every call-site.
        |-P:$name:${pad20(
-        ShowConcreteImplicitTparams
-      )} Shows types in flamegraphs of implicits with concrete type params.
+        GenerateMacroFlamegraph
+      )} Generates a flamegraph for macro expansions. The flamegraph for implicit searches is enabled by default.
+       |-P:$name:${pad20(GenerateProfileDb)}:_ Sets the source root for this project.
+       |-P:$name:${pad20(
+        PrintFailedMacroImplicits
+      )} Prints trees of all failed implicit searches that triggered a macro expansion.
        |-P:$name:${pad20(
         PrintSearchResult
       )}:_ Print implicit search result trees for a list of search ids separated by a comma.
        |-P:$name:${pad20(
-        GenerateMacroFlamegraph
-      )} Generates a flamegraph for macro expansions. The flamegraph for implicit searches is enabled by default.
-       |-P:$name:${pad20(
-        PrintFailedMacroImplicits
-      )} Prints trees of all failed implicit searches that triggered a macro expansion.
+        ShowConcreteImplicitTparams
+      )} Shows types in flamegraphs of implicits with concrete type params.
+       |-P:$name:${pad20(ShowProfiles)} Logs profile information for every call-site.
+       |-P:$name:${pad20(SourceRoot)}:_ Sets the source root for this project.
     """.stripMargin
   )
 
