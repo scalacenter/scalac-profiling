2f916281ec516a215a2e50e3403c107ec1ba5121
danicheg <esik.danny@gmail.com>
2023-12-07 20:27:39 +0300

    Rm scala-sbt-1.0 in sbt-plugin

--- sbt-plugin/src/main/scala-sbt-1.0/sbt/ch/epfl/scala/Compat.scala
+++
@@ -1,7 +0,0 @@
-package sbt.ch.epfl.scala
-
-object Compat {
-  type ExecCommand = sbt.Exec
-  implicit def command2String(command: ExecCommand) = command.commandLine
-  implicit def string2Exex(s: String): ExecCommand = sbt.Exec(s, None, None)
-}
